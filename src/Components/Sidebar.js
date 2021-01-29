572737438e46f49064ce424d53ffa9da0bcab5ee
Bishesh <bishesh.sunam@gmail.com>
2021-01-29 13:29:11 +1300

    code clean up

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,19 +1,14 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { login, logout } from '../features/userSlice';
 import './Sidebar.css';
 
 
 function Sidebar() {
 
-    const dispatch = useDispatch();
-
-
     return (
         <div className="sidebar">
             <div className="sidebar__header">
-                <Avatar onClick={dispatch(logout())}/>
+                <Avatar/>
             </div>
            
             <div className="sidebar__body">
